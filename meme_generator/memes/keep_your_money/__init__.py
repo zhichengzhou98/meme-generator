082320c05500f924ac08330ad887ebfbae8cf146
meetwq <meetwq@gmail.com>
2024-12-31 16:51:24 +0800

    `keep_your_money` enable gif

--- meme_generator/memes/keep_your_money/__init__.py
+++ meme_generator/memes/keep_your_money/__init__.py
@@ -14,6 +14,7 @@ from meme_generator import (
 )
 from meme_generator.exception import MemeFeedback
 from meme_generator.tags import MemeTags
+from meme_generator.utils import make_jpg_or_gif
 
 img_dir = Path(__file__).parent / "images"
 
@@ -44,13 +45,16 @@ def keep_your_money(images: list[BuildImage], texts: list[str], args: Model):
     else:
         raise MemeFeedback("图片编号错误，请输入1或2")
     frame = BuildImage.open(img_dir / f"{number}.png")
-    img = (
-        images[0]
-        .convert("RGBA")
-        .resize((500, 640), keep_ratio=True, inside=True, bg_color="white")
-    )
-    frame.paste(img, (0, 1080 - img.height), alpha=True, below=True)
-    return frame.save_jpg()
+
+    def make(imgs: list[BuildImage]) -> BuildImage:
+        img = (
+            imgs[0]
+            .convert("RGBA")
+            .resize((500, 640), keep_ratio=True, inside=True, bg_color="white")
+        )
+        return frame.copy().paste(img, (0, 1080 - img.height), alpha=True, below=True)
+
+    return make_jpg_or_gif(images, make)
 
 
 add_meme(
@@ -62,5 +66,5 @@ add_meme(
     keywords=["压岁钱不要交给"],
     tags=MemeTags.arona | MemeTags.plana,
     date_created=datetime(2024, 12, 29),
-    date_modified=datetime(2024, 12, 29),
+    date_modified=datetime(2024, 12, 31),
 )
